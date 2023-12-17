5309913b0526c0955eb269026681d33a160b763e
Vishal Poudel <53789228+vishalpoudel@users.noreply.github.com>
2023-12-17 23:42:24 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ratmdatastorevishalpoudelnew2" # Replace with your actual S3 bucket name
+    bucket = "ratmdatastorevishalpoudelnew1" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "us-east-2"
   }
